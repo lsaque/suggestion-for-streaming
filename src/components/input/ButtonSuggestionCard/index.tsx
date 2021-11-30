c4b58cefb9104ad6e9574f61aa8754f34a8ac4f9
João Victor Marks (John) <50274461+JVMarks@users.noreply.github.com>
2021-11-30 20:31:28 -0300

    SignaturePlan
    SignaturePlan for everybody

--- src/components/input/ButtonSuggestionCard/index.tsx
+++ src/components/input/ButtonSuggestionCard/index.tsx
@@ -1,24 +1,21 @@
-import React, { useState } from "react";
-import { Avatar, Box, Button, Chip, useTheme } from "@mui/material";
+import React, { useState } from 'react';
+import { Avatar, Box, Button, Chip, useTheme } from '@mui/material';
 
-import netflixIcon from "../../../assets/images/streamers/icons/netflix.png";
-import primeVideoIcon from "../../../assets/images/streamers/icons/primeVideo.png";
-import globoPlayIcon from "../../../assets/images/streamers/icons/globoPlay.png";
-import conmebolIcon from "../../../assets/images/streamers/icons/conmebol.png";
-import premiereIcon from "../../../assets/images/streamers/icons/premiere.png";
-import SignaturePlanModal from "../../feedback/SignaturePlanModal";
+import netflixIcon from '../../../assets/images/streamers/icons/netflix.png';
+import primeVideoIcon from '../../../assets/images/streamers/icons/primeVideo.png';
+import globoPlayIcon from '../../../assets/images/streamers/icons/globoPlay.png';
+import premiere from '../../../assets/images/streamers/premiere.png';
+import conmebol from '../../../assets/images/streamers/conmebol.png';
+import SignaturePlanModal from '../../feedback/SignaturePlanModal';
 
 interface IButtonSuggestionCardProps {
-  image: string;
-  platformType: number;
-  movieTitle: string;
+  image: string,
+  platformType: 0 | 1 | 2 | 3 | 4 | number,
+  movieTitle: string,
 }
 
-const ButtonSuggestionCard: React.FC<IButtonSuggestionCardProps> = ({
-  image,
-  platformType,
-  movieTitle,
-}) => {
+const ButtonSuggestionCard: React.FC<IButtonSuggestionCardProps> = ({ image, platformType, movieTitle }) => {
+
   const [isModalOpen, setModalOpen] = useState(false);
   const handleSuggestionSubmit = () => setModalOpen(false);
   const theme = useTheme();
@@ -30,56 +27,56 @@ const ButtonSuggestionCard: React.FC<IButtonSuggestionCardProps> = ({
   switch (platformType) {
     case 0:
       platform = netflixIcon;
-      label = "Netflix";
+      label = 'Netflix';
       color = theme.palette.primary.main;
       break;
     case 1:
       platform = primeVideoIcon;
-      label = "Prime Video";
+      label = 'Prime Video';
       color = theme.palette.info.main;
       break;
     case 2:
       platform = globoPlayIcon;
-      label = "Globoplay";
-      color = "#fc4626";
+      label = 'Globoplay';
+      color = '#fc4626';
       break;
     case 3:
-      platform = premiereIcon;
-      label = "Premiere";
-      color = "#2C8F3C";
+      platform = premiere;
+      label = 'Premiere';
+      color = '#338C2E';
       break;
     case 4:
-      platform = conmebolIcon;
-      label = "Conmebol";
-      color = "#05589C";
+      platform = conmebol;
+      label = 'Conmebol';
+      color = '#05589C';
       break;
   }
 
-  const renderAvatar = <Avatar alt="Netflix" src={platform} />;
+  const renderAvatar = <Avatar alt="Netflix" src={platform} />
 
   return (
     <React.Fragment>
       <Button
         onClick={() => setModalOpen(true)}
         sx={{
-          height: "210px",
-          width: "170px",
-          borderRadius: "10px",
+          height: '210px',
+          width: '170px',
+          borderRadius: '10px',
           padding: 0,
         }}
       >
         <Box
           sx={{
             backgroundImage: `url(${image})`,
-            backgroundRepeat: "no-repeat",
-            backgroundPosition: "center",
-            backgroundSize: "cover",
-            width: "100%",
-            height: "100%",
-            borderRadius: "5px",
-            boxShadow: "8px 4px 13px rgba(0,0,0,0.38)",
-            display: "flex",
-            justifyContent: "flex-start",
+            backgroundRepeat: 'no-repeat',
+            backgroundPosition: 'center',
+            backgroundSize: 'cover',
+            width: '100%',
+            height: '100%',
+            borderRadius: '5px',
+            boxShadow: '8px 4px 13px rgba(0,0,0,0.38)',
+            display: 'flex',
+            justifyContent: 'flex-start'
           }}
         >
           <Chip
@@ -88,16 +85,16 @@ const ButtonSuggestionCard: React.FC<IButtonSuggestionCardProps> = ({
             variant="outlined"
             size="small"
             sx={{
-              marginTop: "4px",
-              paddingLeft: "2px",
-              paddingRight: "3px",
-              marginLeft: "4px",
-              textTransform: "capitalize",
-              border: "none",
+              marginTop: '4px',
+              paddingLeft: '2px',
+              paddingRight: '3px',
+              marginLeft: '4px',
+              textTransform: 'capitalize',
+              border: 'none',
               background: color,
-              ".MuiChip-label": {
-                marginTop: "2.75px",
-              },
+              '.MuiChip-label': {
+                marginTop: '2.75px',
+              }
             }}
           />
         </Box>
@@ -105,13 +102,14 @@ const ButtonSuggestionCard: React.FC<IButtonSuggestionCardProps> = ({
 
       <SignaturePlanModal
         // platformId={platformType}
-        platformId={1}
+        platformId={platformType}
         movieTitle={movieTitle}
         isOpenModal={isModalOpen}
         handleModalClose={() => handleSuggestionSubmit()}
       />
+
     </React.Fragment>
   );
-};
+}
 
 export default ButtonSuggestionCard;
